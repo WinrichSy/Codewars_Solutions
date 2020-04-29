6af1c4d7cec47a0a85279948a1ffacea8e5bb150
Winrich Sy <winrichsy@gmail.com>
2020-04-29 10:58:55 -0700

    added more codewars solutions

---
+++ SQL/8kyu/OnTheCanadianBorder.sql
@@ -0,0 +1,6 @@
+--On the Canadian Border
+--https://www.codewars.com/kata/590ba881fe13cfdcc20001b4
+
+SELECT *
+FROM travelers
+WHERE country NOT IN ('Canada', 'Mexico', 'USA')
---
+++ SQL/8kyu/PartyRegistration.sql
@@ -0,0 +1,7 @@
+--Register for the Party
+--https://www.codewars.com/kata/590cc86f7557c0494000007e
+
+INSERT INTO participants(name, age, attending)
+VALUES('dude', 22, TRUE);
+
+SELECT * FROM participants;

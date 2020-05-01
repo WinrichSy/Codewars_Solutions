566071c510c2b526829aa20ea7906913d83f9159
Winrich Sy <winrichsy@gmail.com>
2020-05-01 12:51:17 -0700

    added sql codewars solutions

---
+++ SQL/8kyu/SQLBasics_SimpleSum.sql
@@ -0,0 +1,5 @@
+--SQL Basics: Simple SUM
+--https://www.codewars.com/kata/58110da0009b4f7ef80000ad
+
+SELECT SUM(age) as age_sum
+from people
---
+++ SQL/8kyu/SQLGrasshopper_SelectColumns.sql
@@ -0,0 +1,5 @@
+--SQL Grasshopper: Select Columns
+--https://www.codewars.com/kata/582365c18917435ab3000020
+
+SELECT CUSTID, CUSTNAME, CUSTSTATE
+from customers

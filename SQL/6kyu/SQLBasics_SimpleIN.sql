3efd2b41494aa1efa1aaf56231da7eea24a198fd
Winrich Sy <winrichsy@gmail.com>
2020-05-04 12:28:11 -0700

    more SQL codewars solutions

---
+++ SQL/6kyu/SQLBasics_SimpleIN.sql
@@ -0,0 +1,9 @@
+--Sql Basics: Simple IN
+--https://www.codewars.com/kata/58113c03009b4fcc66000d29
+
+SELECT id, name
+FROM departments
+WHERE id IN
+    (SELECT department_id
+     FROM sales
+     WHERE price>98)
---
+++ SQL/6kyu/SQLBasics_SimpleNULLHandling.sql
@@ -0,0 +1,11 @@
+--SQL Basics: Simple NULL handling
+--
+
+SELECT id,
+        COALESCE(NULLIF(name, ''),'[product name not found]') AS name,
+        price,
+        COALESCE(NULLIF(card_name, ''), '[card name not found]') AS card_name,
+        card_number,
+        transaction_date
+FROM eusales
+WHERE price > 50 and price is not NULL

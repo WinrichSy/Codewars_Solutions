73a9d95d14e11e0a4b2387747aa5d848ce5293c9
Winrich Sy <winrichsy@gmail.com>
2020-05-15 17:25:14 -0700

    added codewars python solutions

---
+++ Python/7kyu/SplitInParts.py
@@ -0,0 +1,6 @@
+#Split In Parts
+#https://www.codewars.com/kata/5650ab06d11d675371000003
+
+def split_in_parts(s, part_length):
+    words = [s[i:i+part_length] for i in range(0, len(s), part_length)]
+    return ' '.join(words)

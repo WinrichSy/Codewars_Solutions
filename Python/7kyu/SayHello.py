2d24ea9b0a75ee9ee8548e064bf1875e0c8f7ebc
Winrich Sy <winrichsy@gmail.com>
2020-06-21 20:52:42 -0700

    more codewars solutions

---
+++ Python/7kyu/SayHello.py
@@ -0,0 +1,10 @@
+#Say Hello!
+#https://www.codewars.com/kata/55955a48a4e9c1a77500005a
+
+def greet(name):
+    if name=='':
+        return None
+    elif name==None:
+        return None
+    else:
+        return "hello {}!".format(name)

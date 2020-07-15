97282ae8d6f6967e7339fc544f79393fb1e964b0
Winrich Sy <winrichsy@gmail.com>
2020-07-14 17:21:49 -0700

    added more python solutions

---
+++ Python/8kyu/Grasshopper_CreateTheRooms.py
@@ -0,0 +1,6 @@
+#Grasshopper - Create the rooms
+#https://www.codewars.com/kata/56a29b237e9e997ff2000048
+
+rooms = {'room1':{'name':{}, 'description':{}, 'completed':{}},
+        'room2':{'name':{}, 'description':{}, 'completed':{}},
+        'room3':{'name':{}, 'description':{}, 'completed':{}}}

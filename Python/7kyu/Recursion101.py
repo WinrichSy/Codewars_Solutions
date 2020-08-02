15027b1fab04ee82432ed766baf53353135b8200
Winrich Sy <winrichsy@gmail.com>
2020-08-01 17:00:29 -0700

    more codewars python solution

---
+++ Python/7kyu/Recursion101.py
@@ -0,0 +1,14 @@
+#Recursion 101
+#https://www.codewars.com/kata/5b752a42b11814b09c00005d
+
+def solve(a,b):
+    if a==0 or b==0:
+        return [a,b]
+    elif a >= 2*b:
+        a = a-(2*b)
+        return solve(a, b)
+    elif b >= 2*a:
+        b = b-(2*a)
+        return solve(a, b)
+    else:
+        return [a,b]
